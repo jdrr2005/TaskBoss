d5c2d05f893bf684e167fe0b26cc1c24653ca803
SebastianJim27 <sebacho2004@gmail.com>
2024-10-02 22:36:53 -0500

    Pruebas en postman
    Ya se elimino por completo tareas y la base de datos es funcional, se hicieron pruebas para crear usuarios en postman las cuales funcionan, pero no crea el token para el modelo de usuario que se creo

--- taskBoss/urls.py
+++ taskBoss/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
+"""taskboss
+taskboss2024"""
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tareas/', include('tareas.urls')),
     path('api/usuarios/', include('usuarios.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-docs'),
-    
 ]
