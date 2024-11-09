aaf28ef8e60eb4e557180d2a5b6ce723319741ac
jdrr2005 <juandavidrodriguezreal2005@gmail.com>
2024-11-08 21:02:50 -0500

    Configuracion parcial envio de correos

---
+++ emails/utils.py
@@ -0,0 +1,16 @@
+from django.core.mail import send_mail
+from django.conf import settings
+
+def sendMailTask(title, assigned_to):
+    asunto = f"Asignación de nueva tarea: {title}"
+    mensaje = f"Se te ha asignado una nueva tarea: {title}. Por favor, revisa la aplicación para más detalles."
+    remitente = settings.EMAIL_HOST_USER
+
+    # Envío de correo
+    send_mail(
+        asunto,
+        mensaje,
+        remitente,
+        [assigned_to],
+        fail_silently=False
+    )
--- tareas/apps.py
+++ tareas/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class TareasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tareas'
+    
+
+    def ready(self):
+        import tareas.signals  # Importa las señales al iniciar la app
