6e3dd17c63793e449a00f373b99262ea4785c8e5
jdrr2005 <juandavidrodriguezreal2005@gmail.com>
2024-11-13 11:42:37 -0500

    prueba base datos

--- taskBoss/settings.py
+++ taskBoss/settings.py
@@ -82,11 +82,21 @@ WSGI_APPLICATION = 'taskBoss.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'localhost',
+        'PORT': 5432,  # Puerto por defecto para SQL Server.
+        'NAME': 'taskboss',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
     }
 }
 
